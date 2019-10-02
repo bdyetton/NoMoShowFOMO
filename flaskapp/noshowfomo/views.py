88e1b51f27b45aea305fb0741bfbb41f4eb139e1
bdyetton@gmail.com <bdyetton@gmail.com>
2019-10-02 10:47:21 -0700

    loading bar done

--- flaskapp/noshowfomo/views.py
+++ flaskapp/noshowfomo/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect
 from noshowfomo import app
 from noshowfomo.models import predict_from_url
 
@@ -18,16 +18,32 @@ from noshowfomo.models import predict_from_url
 def index():
     return render_template("index.html")
 
-@app.route('/noshowfomo')
-def noshowfomo():
-    return render_template("noshowfomo.html")
+@app.route('/nomoshowfomo')
+def nomoshowfomo():
+    return render_template("nomoshowfomo.html")
 
-@app.route('/noshowfomo/go')
-def noshowfomogo():
-  ticketmaster_url = request.args.get('input_url')
-  print('Got-------', ticketmaster_url)
-  will_sell_out, artist, venue, city, country, event_url = predict_from_url(ticketmaster_url)
+@app.route('/nomoshowfomo/slides')
+@app.route('/nomoshowfomo/slides/')
+def slide_link():
+    print('Redirecting to google slides')
+    return redirect('https://docs.google.com/presentation/d/1mMAviK2M_NZML94paKmBVSNm8phbRax7RivjC0Js2Dk/edit?usp=sharing')
 
-  return render_template("noshowfomogo.html", artist=artist,
-                         venue=venue, city=city,
-                         country=country, will_sell_out=will_sell_out, event_url=event_url)
+@app.route('/nomoshowfomo/code')
+@app.route('/nomoshowfomo/code/')
+def code_link():
+    print('Redirecting to github')
+    return redirect('https://github.com/bdyetton/NoMoShowFOMO')
+
+
+@app.route('/nomoshowfomo/go')
+def nomoshowfomo_go():
+    ticketmaster_url = request.args.get('input_url')
+    print('Got-------', ticketmaster_url)
+    try:
+        will_sell_out, artist, venue, city, country, event_url = predict_from_url(ticketmaster_url)
+    except Exception as e:
+        return render_template("nomoshowfomo_ohno.html", error=str(e), event_url=ticketmaster_url)
+    else:
+        return render_template("nomoshowfomo_show.html", artist=artist,
+                               venue=venue, city=city,
+                               country=country, will_sell_out=will_sell_out, event_url=event_url)
